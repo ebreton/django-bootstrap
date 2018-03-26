504177ba3c90ef0df7840a38632d97aa0bd2e27d
ebreton <email>
2018-03-26 20:28:32 +0200

    removed duplicate SERVER_HOST

--- src/myapp/versions.py
+++ src/myapp/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.1.0-12-g52ab7e7'
+_release = '0.1.0-13-g0105cc2'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.1.1'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '52ab7e73bc9ea982abcced151c30254d3b0d5b5c'
+_build = '0105cc281d0705e7e1139c7be3988e2dbf669d58'
