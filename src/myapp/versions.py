52ab7e73bc9ea982abcced151c30254d3b0d5b5c
ebreton <email>
2018-03-26 20:27:01 +0200

    added version

--- src/myapp/versions.py
+++ src/myapp/versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.0-11-gf841892'
+_release = '0.1.0-11-g1793457'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.3.1-rc'
+_version = '0.1.1'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'f841892584589853ddf25eae499041600706fa51'
+_build = '1793457430844e2eb44acf3b4b8de604c8ca26cd'
