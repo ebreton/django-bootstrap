17274243bb0829725af0ccf94b9c121718e107cc
ebreton <email>
2018-03-26 20:28:56 +0200

    Prepared release 0.1.1

--- src/myapp/versions.py
+++ src/myapp/versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.1.0-13-g0105cc2'
+_release = '0.1.0-14-g504177b'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.1.1'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '0105cc281d0705e7e1139c7be3988e2dbf669d58'
+_build = '504177ba3c90ef0df7840a38632d97aa0bd2e27d'
